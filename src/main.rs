859bc123acb777c29d677a8a4983bfe1be31a38a
Morris Hansing <hansing@kth.se>
2020-09-08 00:52:44 +0200

    Reupload of correct reworked main :p

--- src/main.rs
+++ src/main.rs
@@ -1,289 +1,177 @@
-
-/*
-TODO:
- *  PIECE
- *  ==============
- *  COLORS / TEAMS
- *      F: Identity - T: white == white
- *      F: Inverse - T: inverse(white) == black
- *  PIECE TYPES
- *      F: Identity - T: rook == rook
- *  MOVES
- *      F: Diagonal moves - T: -
- *      F: Orthogonal moves - T: -
- *      F: Knights moves - T: -
- *      F: Pawns move - T: -
- *      F: Pawns capture - T: -
- *      F: Promotion move - T: -
- *      F: En passant - T: -
- *      F: Kings move - T: -
- *      F: Castling - T: -
- *
- *  MOVEMENT
- *  ===================
- *  CHECK MOVE LEGALITY
- *      F: Prevent self check.
- *
- *
- *  CHECK CHESS
- *  BOARD
- *  =====
- *      F: "Nullable" - T: --------------------
- *      F: Check legal - T: deny call of (0,8) / (-1, 1) / (0, 8) / (5, -1)
- *
- *
- */
-
-use std::fmt;
-
-fn main(){
-test_bishop_moves()
-}
-
-#[derive(PartialEq, Copy, Clone)]
-enum Color{
+use std::collections::HashMap;
+use std::fmt::Debug;
+#[derive(PartialEq, Debug, Copy, Clone)]
+enum Color {
     White,
-    Black
+    Black,
 }
 
-use Color::White;
-use Color::Black;
-
-impl Color{
-    fn inverse(&self) -> Color{
+impl Color {
+    fn inverse(&self) -> Self {
         match self {
-            White => Black,
-            Black => White
+            Color::Black => Color::White,
+            Color::White => Color::Black,
         }
     }
-
-    fn forwards(&self) -> i8 { //is i8 appropriate?
+    fn forward(&self) -> i32 {
         match self {
-            White => 1,
-            Black => -1
+            Color::Black => -1,
+            Color::White => 1,
         }
     }
 }
 
-impl fmt::Display for Color{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
-        match self {
-            White => write!(f, "White"),
-            Black => write!(f, "Black")
-        }
-    }
-}
-
-#[derive(PartialEq, Copy, Clone)]
-enum PieceType{
+#[derive(PartialEq, Debug, Copy, Clone)]
+enum PieceType {
     Pawn,
     Knight,
     Bishop,
     Rook,
     Queen,
-    King
+    King,
 }
-
-use PieceType::Pawn;
-use PieceType::Knight;
-use PieceType::Bishop;
-use PieceType::Rook;
-use PieceType::Queen;
-use PieceType::King;
-use std::any::Any;
-
-impl fmt::Display for PieceType{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
-        match self {
-            Pawn => write!(f, "Pawn"),
-            Knight=>write!(f, "Knight"),
-            Bishop=>write!(f, "Bishop"),
-            Rook=>write!(f, "Rook"),
-            Queen=>write!(f, "Queen"),
-            King=>write!(f, "King")
-        }
-    }
-}
-
-impl Piece{
-    fn get_standard_movement_rules(&self) -> Vec<(i8, i8, bool)>{
-        match self.piece_type {
-            Pawn    => vec![(self.color.forwards(), 0, false)],
-
-            Knight  => vec![(2, 1, false), (2, -1, false),
-                            (-2, 1, false), (-2, -1, false),
-                            (1, 2, false), (-1, 2, false),
-                            (1, -2, false), (-1, -2, false)],
-
-            Bishop  => vec![(1, 1, true), (-1, -1, true),
-                            (-1, 1, true), (1, -1, true)],
-
-            Rook    => vec![(1, 0, true), (-1, 0, true),
-                            (0, 1, true), (0, -1, true)],
-            Queen   => vec![(1, 0, true), (-1, 0, true),
-                            (0, 1, true), (0, -1, true),
-                            (1, 1, true), (-1, -1, true),
-                            (-1, 1, true), (1, -1, true)],
-
-            King    => vec![(1, 0, false), (-1, 0, false),
-                            (0, 1, false), (0, -1, false),
-                            (1, 1, false), (-1, -1, false),
-                            (-1, 1, false), (1, -1, false)]
-        }
-    }
-}
-
-#[derive(Copy, Clone)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 struct Piece {
     color: Color,
     piece_type: PieceType,
+    has_moved: bool,
 }
 
+
 impl Piece {
-    fn new(color: Color, piece_type: PieceType) -> Self{
+    fn new(color: Color, piece_type: PieceType) -> Self {
         Self {
             color,
             piece_type,
+            has_moved: true,
+        }
+    }
+
+    fn get_move_pattern(&self) -> Vec<(i32, i32, bool)> {
+        //////////////////////////////////////////WHY DOSE IT NOT WORK!!==!=??!?!?!?!?!
+        match self.piece_type {
+            Pawn => vec![(self.color.forward(), 0, false)],
+
+            Knight => vec![
+                (2, 1, false),
+                (2, -1, false),
+                (-2, 1, false),
+                (-2, -1, false),
+                (1, 2, false),
+                (-1, 2, false),
+                (1, -2, false),
+                (-1, -2, false),
+            ],
+
+            Bishop => vec![(1, 1, true), (-1, -1, true), (-1, 1, true), (1, -1, true)],
+
+            Rook => vec![(1, 0, true), (-1, 0, true), (0, 1, true), (0, -1, true)],
+            Queen => vec![
+                (1, 0, true),
+                (-1, 0, true),
+                (0, 1, true),
+                (0, -1, true),
+                (1, 1, true),
+                (-1, -1, true),
+                (-1, 1, true),
+                (1, -1, true),
+            ],
+
+            King => vec![
+                (1, 0, false),
+                (-1, 0, false),
+                (0, 1, false),
+                (0, -1, false),
+                (1, 1, false),
+                (-1, -1, false),
+                (-1, 1, false),
+                (1, -1, false),
+            ],
         }
     }
 }
 
-struct MoveData{
-    from: (usize, usize),
-    to: (usize, usize),
-}
+#[derive(Eq, PartialEq, Debug, Copy, Clone, Hash)]
+struct Space(i32, i32);
 
-impl MoveData{
-    fn new(from: (usize, usize), to: (usize, usize)) -> Self{
-        Self{
-            from,
-            to
-        }
+impl Space {
+    fn is_in_bounds(&self) -> bool {
+        self.0 >= 0 && self.0 < 8 && self.1 >= 0 && self.1 < 8
+    }
+    fn offset(&self, rank_offset: i32, file_offset: i32) -> Self {
+        Self(self.0 + rank_offset, self.1 + file_offset)
     }
 }
 
-impl fmt::Display for MoveData{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
-        write!("{}{}")
-    }
+struct GameState {
+    board: HashMap<Space, Piece>,
+    captured: Vec<Piece>,
 }
 
-struct BoardState {
-    board: [[Option<Piece>; 8]; 8],
-    prisoners: Vec<Piece>,
-    to_play: Color,
-}
-
-impl BoardState {
-    fn new_blank() -> Self{
+impl GameState {
+    fn new_blank() -> Self {
         Self {
-            board: [[None; 8]; 8],
-            prisoners: Vec::new(),
-            to_play: White
+            board: HashMap::new(),
+            captured: Vec::new(),
         }
     }
-    fn check_standard_move_legality(&self, to: (i8, i8), piece: &Piece) -> bool{
-        if !is_in_bounds(to) {
-            return false;
-        }
-        let to_content = self.board[to.0 as usize][to.1 as usize];
-        if to_content.is_some() && (to_content.unwrap().color == piece.color || piece.piece_type == Pawn){
-            return false;
-        }
-        true
-    }
-    fn get_action_space(){
 
-    }
-    fn get_piece_action_space(&self, from: (usize, usize), piece: &Piece) -> Vec<MoveData>{ //MIGHT NEED TO BE OPTION
-    //Rook movement
-        let mut action_space = Vec::new();
-        let standard_move_rules = piece.get_standard_movement_rules();
-        for sm in standard_move_rules{ //TODO consider names
-            //let mut to: (i8, i8) = (from.0 + sm.0, from.1 + sm.1);
-            for i in 1..8i8 {
-                let to: (i8, i8) = (from.0 as i8 + sm.0 * i, from.1 as i8 + sm.1 * i);
-                if self.check_standard_move_legality(to, piece) { break; }
-                action_space.push(MoveData::new(from, (to.0 as usize, to.1 as usize)));
-                if !sm.2 { break; }
-            }
-        }
-        action_space
+    fn get_piece_actionspace(&self, space: &Space) {
+        // WHY "&" HERE??????
+        let piece = self.board[space];
     }
 }
 
-
-fn is_in_bounds(space: (i8, i8)) -> bool{
-    space.0 >= 0 &&
-        space.0 < 8 &&
-        space.1 >= 0 &&
-        space.1 < 8
-}
-
-
+/* TESTS
+ * =====
+ * Color
+ * -inverse
+ * PieceType
+ * Piece
+ * Space
+ * -check_in_bounds
+ * GameState
+ * -Board
+ * -Captured
+ */
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
     #[test]
-    fn test_color(){
-
-    //COLOR EQUALS
-    assert!(White == White);
-    assert!(White != Black);
-    //COLOR INVERSE
-    assert!(White.inverse() == Black);
-    assert!(Black.inverse() == White);
-    let w = White;
-    assert!(w.inverse() == Black);
-    assert!(w.inverse() == Black);
-    //COLOR DISPLAY
-    assert_eq!(format!("{}", White), "White");
-    assert_eq!(format!("{}", Black), "Black");
-    }
-
-    #[test]
-    fn test_piece_type() {
-        // TYPE EQUALS
-        assert!(Pawn == Pawn);
-        assert!(Bishop != Knight);
-        // TYPE DISPLAY
-        assert_eq!(format!("{}", Knight), "Knight");
-        assert_eq!(format!("{}", King), "King");
-    }
-
-    #[test]
-    fn test_board(){
-        let mut blank_board = BoardState::new_blank();
-        assert!(blank_board.board[4][5].is_none());
-        blank_board.prisoners.push(Piece::new(White, Queen));
-        assert!(blank_board.prisoners[0].color == White);
-
-        //blank_board.board[0][0] == Piece::new(Black, King); //SO FAR: NO NEED FOR PIECE EQUALS
-
+    fn color_equals() {
+        assert_eq!(Color::White, Color::White);
     }
     #[test]
-    #[should_panic]
-    fn test_board_oob(){
-        let mut blank_board = BoardState::new_blank();
-        let out_of_bounds = blank_board.board[0][9];
+    fn color_inverse() {
+        assert_eq!(Color::White.inverse(), Color::Black);
     }
     #[test]
-    fn test_bishop_moves(){
-        let mut board = BoardState::new_blank();
-        board.board[5][5] = Some(Piece::new(White, Bishop));
-        assert!(board.get_piece_action_space((5,5),board.board[5][5].unwrap()));
-
+    fn piece_type_equals() {
+        assert_eq!(PieceType::Bishop, PieceType::Bishop);
     }
-
+    #[test]
+    fn out_of_bounds_space() {
+        assert!(!Space(-1, 3).is_in_bounds());
+    }
+    #[test]
+    fn space_eq() {
+        assert_eq!(Space(1, 1), Space(1, 1));
+    }
+    #[test]
+    fn space_offset() {
+        assert_eq!(Space(4, 6), Space(1, 1).offset(3, 5));
+    }
+    #[test]
+    fn board_test(){
+        let s = Space(2,5);
+        let p = Piece::new(Color::White, PieceType::Knight);
+        let mut g = GameState::new_blank();
+        g.board.insert(s, p);
+        assert_eq!(p, g.board[&s]);
+    }
 }
-fn test_bishop_moves(){
-        let mut board = BoardState::new_blank();
-        board.board[5][5] = Some(Piece::new(White, Bishop));
-        //assert!(board.get_piece_action_space((5,5),board.board[5][5].unwrap()));
-    for i in board.get_piece_action_space((5,5),&board.board[5][5].unwrap()) {
-        println!("{:?}", i)
-    }
 
-    }
-
+fn main() {
+    println!()
+}
